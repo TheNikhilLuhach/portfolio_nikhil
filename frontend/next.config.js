eb1152d7f09eaeda441e80c75e0d7ec556c624ad
TheNikhilLuhach <nikuluhach.86209@gmail.com>
2026-03-21 02:50:33 +0530

    cleanup: Final cleanup for Vercel Next.js deployment

--- frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: { unoptimized: true },
-  poweredByHeader: false,
-};
-
-module.exports = nextConfig;
